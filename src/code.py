660e3c9994cf5681dd279b1679a34a7fc42d4f9c
taufiqhusada <taufiqhusdr@gmail.com>
2020-05-29 20:27:39 +0700

    done milestone 1

---
+++ src/code.py
@@ -0,0 +1,101 @@
+import heapq 
+from math import inf, isfinite
+
+mapNode = {}
+
+def findShortestPath(a, b, adjMatrixAll):
+    path = []
+    # djikstra from a 
+    # (berdasarkan referensi psudocode dari buku "Pemrograman Kompetitif Dasar")
+    dist = [inf for i in range(6105)]
+    pred = [-1 for i in range(6105)]
+    isVisited = [False for i in range(6105)]
+
+    pq = []
+    heapq.heapify(pq)
+    heapq.heappush(pq,(0,a))
+    dist[a] = 0
+    while(len(pq)!=0):
+        (curDist, u) = heapq.heappop(pq)
+        if (not isVisited[u]):
+            isVisited[u] = True
+            for v in range(6105):
+                if (isfinite(adjMatrixAll[u][v])):
+                    if (dist[v] > dist[u] + adjMatrixAll[u][v]):
+                        dist[v] = dist[u] + adjMatrixAll[u][v]
+                        pred[v] = u
+                        heapq.heappush(pq, (dist[v],v))
+    
+    # find path dari a -> b
+    nodeNow = b
+    while(nodeNow!=-1):
+        path.append(nodeNow)
+        nodeNow = pred[nodeNow]
+    path = path.reverse()
+    # print(path)
+    return dist[b], path
+
+def getEdgeDataFromFile():
+    adjMatrixAll = [[inf for i in range(6105)] for i in range(6105)]
+    fEdge = open('../data/EdgeData.txt', 'r').readlines()
+    for i in range(6105):
+        for j in range(6105):
+            if(i==j):
+                adjMatrixAll[i][j] = 0
+
+    for line in fEdge:
+        parseLine = [x for x in line.split()]
+        u = int(parseLine[1])
+        v = int(parseLine[2])
+        weight = float(parseLine[3])
+        adjMatrixAll[u][v] = weight
+    
+    return adjMatrixAll
+
+def initSubGraph(nodeKantor, listNodeTujuan, adjMatrixAll):
+    listPath = [[[] for i in range(len(listNodeTujuan)+1)] for e in range(len(listNodeTujuan)+1)]
+    adjMatrixSubGraph = [[inf for i in range(len(listNodeTujuan)+1)] for e in range(len(listNodeTujuan)+1)]
+
+    listNodeTujuan.append(nodeKantor)   #sementara aja
+
+    for u in listNodeTujuan:
+        for v in listNodeTujuan:
+            uMapped = mapNode[u]
+            vMapped = mapNode[v]
+            if (u==v):
+                adjMatrixSubGraph[uMapped][vMapped] = 0
+                listPath[uMapped][vMapped] = [u]
+            else:
+                dist, path = findShortestPath(u,v,adjMatrixAll)
+                # print(dist)
+                adjMatrixSubGraph[uMapped][vMapped] = dist
+                listPath[uMapped][vMapped] = path
+
+    listNodeTujuan.pop()
+
+    return adjMatrixSubGraph, listPath
+
+def init():
+    print('masukkan simpul kantor pusat:', end=' ')
+    nodeKantor = int(input())
+    print('masukkan jumlah titik tujuan:', end='')
+    nTujuan = int(input())
+    print('masukkan titik-titik tujuan (pisahkan dengan spasi)')
+    listNodeTujuan = [int(x) for x in input().split()]
+    
+    #node mapping
+    mapNode[nodeKantor] = 0
+    for i in range(nTujuan):
+        mapNode[listNodeTujuan[i]] = i+1
+
+    adjMatrixAll = getEdgeDataFromFile()
+    
+    adjMatrixSubGraph, listPath = initSubGraph(nodeKantor, listNodeTujuan, adjMatrixAll)
+    
+    # print
+    listNodeTujuan.append(nodeKantor)
+    for u in listNodeTujuan:
+        print(u, " ", adjMatrixSubGraph[mapNode[u]])
+
+if __name__ == "__main__":
+    init()
